fb7cba780954ab69d05d24b6869308644b731a45
Leap of Azzam <leapofazzam@gmail.com>
2021-07-21 19:20:25 +0700

    headers

---
+++ src/include/kernel/fb.h
@@ -0,0 +1,39 @@
+#include <stivale/stivale2.h>
+
+// stivale2 offers a runtime terminal service which can be ditched at any
+// time, but it provides an easy way to print out to graphical terminal,
+// especially during early boot.
+// Read the notes about the requirements for using this feature below this
+// code block.
+static struct stivale2_header_tag_terminal terminal_hdr_tag = {
+    // All tags need to begin with an identifier and a pointer to the next tag.
+    .tag = {
+        // Identification constant defined in stivale2.h and the specification.
+        .identifier = STIVALE2_HEADER_TAG_TERMINAL_ID,
+        // If next is 0, it marks the end of the linked list of header tags.
+        .next = 0
+    },
+    // The terminal header tag possesses a flags field, leave it as 0 for now
+    // as it is unused.
+    .flags = 0
+};
+ 
+// We are now going to define a framebuffer header tag, which is mandatory when
+// using the stivale2 terminal.
+// This tag tells the bootloader that we want a graphical framebuffer instead
+// of a CGA-compatible text mode. Omitting this tag will make the bootloader
+// default to text mode, if available.
+static struct stivale2_header_tag_framebuffer framebuffer_hdr_tag = {
+    // Same as above.
+    .tag = {
+        .identifier = STIVALE2_HEADER_TAG_FRAMEBUFFER_ID,
+        // Instead of 0, we now point to the previous header tag. The order in
+        // which header tags are linked does not matter.
+        .next = (uint64_t)&terminal_hdr_tag
+    },
+    // We set all the framebuffer specifics to 0 as we want the bootloader
+    // to pick the best it can.
+    .framebuffer_width  = 0,
+    .framebuffer_height = 0,
+    .framebuffer_bpp    = 0
+};
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stddef.h>
-#include "stivale2.h"
+#include <stivale/stivale2.h>
+#include <kernel/fb.h>
  
 // We need to tell the stivale bootloader where we want our stack to be.
 // We are going to allocate our stack as an uninitialised array in .bss.
@@ -9,45 +10,7 @@ static uint8_t stack[4096];
 // stivale2 uses a linked list of tags for both communicating TO the
 // bootloader, or receiving info FROM it. More information about these tags
 // is found in the stivale2 specification.
- 
-// stivale2 offers a runtime terminal service which can be ditched at any
-// time, but it provides an easy way to print out to graphical terminal,
-// especially during early boot.
-// Read the notes about the requirements for using this feature below this
-// code block.
-static struct stivale2_header_tag_terminal terminal_hdr_tag = {
-    // All tags need to begin with an identifier and a pointer to the next tag.
-    .tag = {
-        // Identification constant defined in stivale2.h and the specification.
-        .identifier = STIVALE2_HEADER_TAG_TERMINAL_ID,
-        // If next is 0, it marks the end of the linked list of header tags.
-        .next = 0
-    },
-    // The terminal header tag possesses a flags field, leave it as 0 for now
-    // as it is unused.
-    .flags = 0
-};
- 
-// We are now going to define a framebuffer header tag, which is mandatory when
-// using the stivale2 terminal.
-// This tag tells the bootloader that we want a graphical framebuffer instead
-// of a CGA-compatible text mode. Omitting this tag will make the bootloader
-// default to text mode, if available.
-static struct stivale2_header_tag_framebuffer framebuffer_hdr_tag = {
-    // Same as above.
-    .tag = {
-        .identifier = STIVALE2_HEADER_TAG_FRAMEBUFFER_ID,
-        // Instead of 0, we now point to the previous header tag. The order in
-        // which header tags are linked does not matter.
-        .next = (uint64_t)&terminal_hdr_tag
-    },
-    // We set all the framebuffer specifics to 0 as we want the bootloader
-    // to pick the best it can.
-    .framebuffer_width  = 0,
-    .framebuffer_height = 0,
-    .framebuffer_bpp    = 0
-};
- 
+  
 // The stivale2 specification says we need to define a "header structure".
 // This structure needs to reside in the .stivale2hdr ELF section in order
 // for the bootloader to find it. We use this __attribute__ directive to
