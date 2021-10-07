274619273fcadc308f86bb02a87919ec85ab8a3c
Leap of Azzam <leapofazzam@gmail.com>
2021-10-07 13:30:28 +0700

    nah f*cker let's scrap tss for now

--- src/include/kernel/gdt.h
+++ src/include/kernel/gdt.h
@@ -1,21 +1,21 @@
 #pragma once
 #include <stdint.h>
 
-struct gdt_desc_t {
+typedef struct __attribute__((packed)) {
     uint16_t size;
     uint64_t offset;
-} __attribute__((packed));
+} gdt_desc_t;
 
-struct gdt_entry_t {
+typedef struct __attribute__((packed)) {
     uint16_t limit0;
     uint16_t base0;
     uint8_t base1;
     uint8_t access;
     uint8_t limit1_flags;
     uint8_t base2;
-} __attribute__((packed));
+} gdt_entry_t;
 
-struct gdt_t {
+typedef struct __attribute__((packed)) {
     struct gdt_entry_t null;
     struct gdt_entry_t kernel_code;
     struct gdt_entry_t kernel_data;
@@ -23,8 +23,7 @@ struct gdt_t {
     struct gdt_entry_t user_code;
     struct gdt_entry_t user_data;
     struct gdt_entry_t reserved;
-} __attribute__((packed))
-__attribute((aligned(0x1000)));
+} gdt_t;
 
 void gdt_init(void);
 extern void gdt_flush(struct gdt_desc_t *gdt_desc);
---
+++ src/include/kernel/pic.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void pic_remap(void);
